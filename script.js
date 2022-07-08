0f83623d81b1015eee89c58d0eb0a1eaf3105a39
Eternity <eternity@thezombiecoder.com>
2022-07-08 14:34:33 -0700

    images for main content

---
+++ script.js
@@ -0,0 +1,11 @@
+const searchLink = document.querySelector(".search-link");
+const searchContainer = document.querySelector("#search-container");
+const exitLink = document.querySelector(".exit-container");
+
+searchLink.addEventListener("click", function () {
+  searchContainer.style.display = "flex";
+});
+
+exitLink.addEventListener("click", function () {
+  searchContainer.style.display = "none";
+});
